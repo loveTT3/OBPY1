bcbd9503a259c9701c05eb52593dfa3bb201ff85
yangjialong <17611251153@163.com>
2020-06-21 19:12:55 +0800

    json

--- 006_模块/a06_sys_os_demo/demo_02_sys.py
+++ 006_模块/a06_sys_os_demo/demo_02_sys.py
@@ -1,1 +1,14 @@
 # sys
+# 和python解释器相关的操作
+
+
+
+#获取命令行
+import sys
+
+# 返回的是字符串  
+# print('脚本名：' ,sys.argv[0])
+# print('第一个参数：' ,sys.argv[1])
+# print('第二个参数：' ,sys.argv[2])
+
+print(int(sys.argv[1])+int(sys.argv[2]))
